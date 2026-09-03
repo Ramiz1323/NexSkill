eb3ea6cfb015b6ab94376bd30d84338cd149bd2f
mohdsaifansari191-art <mohdsaifansari191@gmail.com>
2026-09-04 00:36:34 +0530

    feat(forecasting): implement session 3 future-ready demand forecasting with RTK, multi-year projections and automation risk indicators

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import marketReducer from './slices/marketSlice';
 import curriculumReducer from './slices/curriculumSlice';
+import demandReducer from './slices/demandSlice';
 
 export const store = configureStore({
   reducer: {
     market: marketReducer,
     curriculum: curriculumReducer,
+    demand: demandReducer,
   },
 });
 
